c7056e7a4c9bb70cfd43ef80186af04de19468b3
HalomkoDmytro <galomko.dmytro@gmail.com>
2025-06-11 20:16:29 +0300

    pagination

--- src/component/pagination/pagination.js
+++ src/component/pagination/pagination.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
 
-const Pagination = ({first, from, to, last, currentNumber, updatePage}) => {
+const Pagination = ({first, totalPages, last, pageNumber, updatePage}) => {
 
     const listPaginationElement = () => {
         let res = [];
-
-        for(let i = from; i<=to; i++) {
-            let isActive = i === currentNumber ? 'active' : '';
+        let from = 0 > pageNumber - 2 ? 0 : pageNumber - 2;
+        const maxPages = 8;
+        let to = totalPages > from + maxPages ? from + maxPages : totalPages;
+        for(let i = from; i < to; i++) {
+            let isActive = (i)=== pageNumber ? 'active' : '';
             res.push(
                 <li className={`page-item ${isActive}`} key={i}>
                     <a className="page-link"
-                        onClick={(e) => {updatePage(i)}}>{i}</a>
+                        onClick={(e) => {updatePage(i)}}>{i+1}</a>
                 </li>
             )
         }
@@ -18,18 +20,20 @@ const Pagination = ({first, from, to, last, currentNumber, updatePage}) => {
         return res;
     }
 
+    const isFirstDisabled = () => first ? 'disabled' : '';
+    const isLastDisabled = () => last ? 'disabled' : '';
 
     return (
         <div>
             <ul className="pagination">
-                <li className="page-item disabled">
-                    <a className="page-link" href="#">&laquo;</a>
+                <li className={`page-item ${isFirstDisabled()}`} >
+                    <a className="page-link" onClick={(e) => {updatePage(0)}}>&laquo;</a>
                 </li>
 
                 {listPaginationElement()}
 
-                <li className="page-item">
-                    <a className="page-link" href="#">&raquo;</a>
+                <li className={`page-item ${isLastDisabled()}`} >
+                    <a className="page-link" onClick={(e) => {updatePage(totalPages)}}>&raquo;</a>
                 </li>
             </ul>
         </div>
