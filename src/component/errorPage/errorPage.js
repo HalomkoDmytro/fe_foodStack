7029cd20a37ca9ca8fd448a00fe7cdcbdaf27bea
HalomkoDmytro <galomko.dmytro@gmail.com>
2025-06-13 13:12:44 +0300

    update error page

--- src/component/errorPage/errorPage.js
+++ src/component/errorPage/errorPage.js
@@ -5,12 +5,20 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" style={{ fontFamily: 'canvas',
+                                    fontSize: '24px',
+                                    margin: '25px',
+                                    width: '100%'}} >
+      <div style={{textAlign: 'center'}}>
+                <h1 style={{textAlign: 'center'}}>Oops!</h1>
+                <img src={'/img/error_page.png'} style={{maxWidth: '300px'}} alt="img should be here"/>
+                <p>Sorry, an unexpected error has occurred.</p>
+                <p>
+                  <i>{error.statusText || error.message}</i>
+                </p>
+
+      </div>
+
     </div>
   );
 }
