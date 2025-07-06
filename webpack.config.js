dfdfba74772e18b9dc2b00a424942453ae77084f
HalomkoDmytro <galomko.dmytro@gmail.com>
2025-07-06 09:58:38 +0300

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,96 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MinCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: "development",
+module.exports = (env = {}) => {
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[contenthash].js',
-        clean: true
-    },
+    const {mode = 'development'} = env;
+    const isProd = mode === 'production';
 
-    module: {
-        rules: [
-            // Loading images
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(css)$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            // Loading images
-            {
-                test: /\.(png|jpg|jpeg|gif|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name]-[sha1:hash:7].[ext]'
-                        }
-                    }
-                ]
-            },
-            // Loading fonts
-            {
-                test: /\.(ttf|otf|eot|woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts',
-                            name: '[name].[ext]'
-                        }
-                    }
-                ]
-            }
+    const getStyleLoader = () => {
+        return [
+            isProd ?  MinCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
         ]
-    },
+    };
 
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-              { from: 'public/bootstrap.min.css', to: 'bootstrap.min.css' }
+    const getPlugins = () => {
+        const plugins = [
+              new CopyWebpackPlugin({
+                  patterns: [
+                    { from: 'public/bootstrap.min.css', to: 'bootstrap.min.css' }
+                  ]
+                }),
+              new HtmlWebpackPlugin({
+                  template: './public/index.html',
+                  inject: 'body',
+            }),
+        ];
+
+        if(isProd) {
+            plugins.push(new MinCssExtractPlugin({
+                filename: 'main-[hash:8].css'
+            }))
+        }
+
+        return plugins;
+    }
+
+    return {
+        mode: isProd ? 'production' : 'development',
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isProd ? 'main-[hash:8].js' : 'bundle.[contenthash].js',
+            clean: true
+        },
+
+        module: {
+            rules: [
+                // Loading images
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.(css)$/i,
+                    use: getStyleLoader()
+                },
+                // Loading images
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
+                        }
+                    ]
+                },
+                // Loading fonts
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]'
+                            }
+                        }
+                    ]
+                }
             ]
-          }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            inject: 'body',
-        }),
-    ],
+        },
 
+        plugins: getPlugins(),
+
+        devServer: {
+            open: true,
+        }
+    }
 }
